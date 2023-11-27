54210a04e535ef95620eea8c6e3d0c9afd0565e5
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-11-27 20:04:05 -0300

    chore: add handleSubmit function

--- src/components/recordForm/RecordForm.jsx
+++ src/components/recordForm/RecordForm.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './RecordForm.css'
 
 export default function RecordForm() {
+    //Variáveis que representam os dados de entrada
+    const [name, setName] = useState('')
+    const [value, setValue] = useState('');
+    const [type, setType] = useState('')
+    const [payment, setPayment] = useState('')
 
+
+    // Parâmetros: (e) - captura o evento
+    const handleSubmission = (e) => {
+        // Impede que o formulário seja enviado da maneira tradicional
+        e.preventDefault();
+  
+        // Caso algum dos campos estejam vazios, não continua a função
+        if(!name || !value || !type || !payment){
+          window.alert('Há campos vazios!')
+          return;
+        } 
+    }
 
 
   return (
-    <form>
+    <form onSubmit={handleSubmission}>
         <span>
             <label htmlFor="name">Nome:</label>
             <input type="text" id="name" name="name" />
@@ -18,6 +35,7 @@ export default function RecordForm() {
         <span>
             <label htmlFor="type">Tipo:</label>
             <select id="type" name="type">
+            <option value="income">Selecione a categoria</option>
                 <option value="income">Receita</option>
                 <option value="expense">Dispesa</option>
             </select>
@@ -25,6 +43,7 @@ export default function RecordForm() {
         <span>
             <label for="paymentMethod">Pagamento:</label>
             <select id="paymentMethod" name="paymentMethod">
+                <option value="income">Método de pagamento</option>
                 <option value="creditCard">Cartão de Crédito</option>
                 <option value="debitCard">Cartão de Débito</option>
                 <option value="pix">Pix</option>
@@ -33,9 +52,6 @@ export default function RecordForm() {
             </select>
         </span>
         <span className='button_wrapper'><button type="submit">Adicionar</button></span>
-       
-
-
     </form>
   )
 }

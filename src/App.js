ea59f375845a3d6a3b003604b94ec48d7d12c463
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-11-27 19:32:44 -0300

    feat: add sections to register, income and expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <section className='section record_wrapper'>
+      <h1>Cadastro</h1>
+
+      </section>
+      <section className='section income_wrapper'>
+      <h1>Receita</h1>
+
+      </section>
+      <section className='section expense_wrapper'>
+      <h1>Despesa</h1>
+
+      </section>
     </div>
   );
 }
